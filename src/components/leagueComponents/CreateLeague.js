e9806c2dbc4c4b4f1e021abd0bd2be960c447544
johnrusch <johnrusch7@gmail.com>
2020-10-24 12:46:01 -0700

    makes create league and success modal work

--- src/components/leagueComponents/CreateLeague.js
+++ src/components/leagueComponents/CreateLeague.js
@@ -2,27 +2,18 @@ import React, { useState, useEffect } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import Button from "@material-ui/core/Button";
 import Modal from "@material-ui/core/Modal";
-import ListItem from "@material-ui/core/ListItem";
-import TextField from "@material-ui/core/TextField";
-import FormControl from "@material-ui/core/FormControl";
-import Select from "@material-ui/core/Select";
-import InputLabel from "@material-ui/core/InputLabel";
-import MenuItem from "@material-ui/core/MenuItem";
 import { ValidatorForm, TextValidator } from "react-material-ui-form-validator";
 
 import { connect } from "react-redux";
-import { createLeague } from '../../actions/leagueActions'
-import LeagueSuccessModal from "./LeagueSuccessModal";
-
-
+import { createLeague } from "../../actions/leagueActions";
 
 const CreateLeague = (props) => {
-  const [open, setOpen] = useState(false);
+  //new league state
   const [name, setName] = useState("");
   const [password, setPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
-  const [teams, setTeams] = useState("");
 
+  //collects new league into object for post request
   const newLeague = {
     name: name,
     passphrase: password,
@@ -30,6 +21,7 @@ const CreateLeague = (props) => {
     creator_id: props.currentUserId,
   };
 
+  //new league state handlers
   const updateName = (username) => {
     return setName(username);
   };
@@ -42,61 +34,123 @@ const CreateLeague = (props) => {
     return setConfirmPassword(leaguePassword);
   };
 
+  //onSubmit event handler
   const addLeague = (e) => {
     e.preventDefault();
-    // console.log(newLeague);
     props.createLeague(newLeague);
-    if (!props.errors) return <LeagueSuccessModal {...props} />
+    if (!props.errors) {
+      return handleOpen();
+    } else {
+      return props.error;
+    }
   };
 
+  //on component update checks to see if passwords match
   useEffect(() => {
-    ValidatorForm.addValidationRule('isPasswordMatch', (value) => {
+    ValidatorForm.addValidationRule("isPasswordMatch", (value) => {
       if (value !== password) {
-          return false;
+        return false;
       }
       return true;
+    });
   });
-  })
+
+  //modal state
+  const [open, setOpen] = useState(false);
+  const [modalStyle] = useState(getModalStyle);
+
+  //modal styling
+  function rand() {
+    return Math.round(Math.random() * 20) - 10;
+  }
+
+  function getModalStyle() {
+    const top = 50 + rand();
+    const left = 50 + rand();
+    return {
+      top: `${top}%`,
+      left: `${left}%`,
+      transform: `translate(-${top}%, -${left}%)`,
+    };
+  }
+
+  const useStyles = makeStyles((theme) => ({
+    modal: {
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "center",
+    },
+    paper: {
+      position: "absolute",
+      width: 450,
+      backgroundColor: theme.palette.background.paper,
+      boxShadow: theme.shadows[5],
+      padding: theme.spacing(2, 4, 3),
+    },
+    formControl: {
+      margin: theme.spacing(1),
+      minWidth: 120,
+    },
+    selectEmpty: {
+      marginTop: theme.spacing(2),
+    },
+  }));
+  
+  const classes = useStyles();
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
 
   return (
     <div>
-      
-        <div>
-          {/* <h2>Create a new league</h2> */}
-          <ValidatorForm
-            onSubmit={addLeague}
-          >
-            <TextValidator
-              id="leagueName"
-              label="Name"
-              value={name}
-              onChange={(e) => updateName(e.target.value)}
-              validators={["required"]}
-              errorMessages={["this field is required"]}
-            />
-            <TextValidator
-              id="leaguePassword"
-              label="Password"
-              type="password"
-              value={password}
-              onChange={(e) => updatePassword(e.target.value)}
-              validators={["required"]}
-              errorMessages={["this field is required"]}
-            />
-            <TextValidator
-              id="confirmLeaguePassword"
-              label="Confirm Password"
-              type="password"
-              value={confirmPassword}
-              onChange={(e) => updateConfirmPassword(e.target.value)}
-              validators={["required", 'isPasswordMatch']}
-              errorMessages={["this field is required", 'password mismatch']}
-            />
-            <Button variant="contained" color="primary" type="submit">
-              Create League
-            </Button>
-          </ValidatorForm>
-        </div>
+      <ValidatorForm onSubmit={addLeague}>
+        <TextValidator
+          id="leagueName"
+          label="Name"
+          value={name}
+          onChange={(e) => updateName(e.target.value)}
+          validators={["required"]}
+          errorMessages={["this field is required"]}
+        />
+        <TextValidator
+          id="leaguePassword"
+          label="Password"
+          type="password"
+          value={password}
+          onChange={(e) => updatePassword(e.target.value)}
+          validators={["required"]}
+          errorMessages={["this field is required"]}
+        />
+        <TextValidator
+          id="confirmLeaguePassword"
+          label="Confirm Password"
+          type="password"
+          value={confirmPassword}
+          onChange={(e) => updateConfirmPassword(e.target.value)}
+          validators={["required", "isPasswordMatch"]}
+          errorMessages={["this field is required", "password mismatch"]}
+        />
+        <Button variant="contained" color="primary" type="submit">
+          Create League
+        </Button>
+      </ValidatorForm>
+      <div>
+        <Modal
+          aria-labelledby="simple-modal-title"
+          aria-describedby="simple-modal-description"
+          open={open}
+          onClose={handleClose}
+        >
+          <div style={modalStyle} className={classes.paper}>
+            You're ready to go!
+          </div>
+        </Modal>
+      </div>
     </div>
   );
 };
@@ -104,7 +158,7 @@ const CreateLeague = (props) => {
 const mapStateToProps = (state) => {
   return {
     currentUserId: state.auth.id,
-    errors: state.errors
+    errors: state.errors,
   };
 };
 
