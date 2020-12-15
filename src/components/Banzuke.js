ba6a89b626682deca520f88df7ac34c01f065078
johnrusch <johnrusch7@gmail.com>
2020-12-15 11:30:56 -0800

    going to lunch

--- src/components/Banzuke.js
+++ src/components/Banzuke.js
@@ -1,7 +1,15 @@
 import React from 'react';
 import { observer } from 'mobx-react';
+import Paper from "@material-ui/core/Paper";
+import { useStore } from '../store';
 
 const Banzuke = observer((props) => {
+    const store = useStore()
+    const wrestlers = store.wrestlers
+
+    const findWrestler = (division, rank) => {
+        
+    }
 
     return (
         <div>
