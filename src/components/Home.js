14e3637df0d9eccc88767ea658ce52143459c44c
johnrusch <johnrusch7@gmail.com>
2021-01-04 14:20:42 -0800

    thangs' :

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,7 @@ import { observer } from "mobx-react";
 import { useStore } from "../store";
 import Typography from "@material-ui/core/Typography";
 import Paper from "@material-ui/core/Paper";
-import Banzuke from './Banzuke';
+import Banzuke from "./Banzuke";
 
 const Home = observer((props) => {
   const store = useStore();
@@ -11,11 +11,15 @@ const Home = observer((props) => {
   return (
     <div className="homeContainer">
       <div className="homeHeader">
-        <Paper><Typography variant="h3">Welcome back, {store.currentUserName}!</Typography></Paper>
-        </div>
+        <Paper>
+          <Typography variant="h3">
+            Welcome back, {store.currentUserName}!
+          </Typography>
+        </Paper>
+      </div>
       <div className="homeLeftColumn">
         <Paper>
-
+          <Typography style={{textAlign: "center"}}>Sumo news coming soon!</Typography>
         </Paper>
       </div>
       <div className="homeBanzuke">
